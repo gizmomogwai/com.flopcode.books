a4339c5fdc1e91b555f5f24963cc6d7de9282812
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-12 18:38:30 +0100

    fixes #3 added profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :books
   has_many :checkouts
   has_many :api_keys
+
+  def create_api_key(for_app)
+    api_key = ApiKey.new
+    api_key.user = self
+    api_key.for_app = for_app
+    api_key.save
+    return api_key
+  end
 end
