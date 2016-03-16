84df4a2706008868e2a2208ade807af53c01034b
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-16 18:02:57 +0100

    steps on providing checkout/checkin api

--- server/app/models/active_checkout.rb
+++ server/app/models/active_checkout.rb
@@ -2,17 +2,27 @@ class ActiveCheckout < ActiveRecord::Base
   belongs_to :book
   belongs_to :checkout
   def self.for(user, book)
+    raise 'book already taken' if book.active_checkout
+
     ac = ActiveCheckout.new
     ac.book = book
-    
+
     c = Checkout.new
     c.from = DateTime.new
     c.user = user
     c.book = book
-    
+
     ac.checkout = c
-    
+
     ac.save
     ac
+  end
+
+  def release(user)
+    raise 'wrong user' unless user == checkout&.user
+
+    checkout.to = DateTime.new
+    checkout.save
+    destroy
   end
 end
