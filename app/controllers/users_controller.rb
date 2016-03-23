78da4ac12e078f4e79d8fc113db9698068161451
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-23 07:23:53 +0100

    better coverage for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,57 +25,44 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
-    check_user unless @logged_in_user&.admin
+    check_user {
+      respond_with @user
+    }
   end
 
   # POST /users
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user, notive: 'User was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /users/1
   def update
     @user = User.find(params[:id])
-    check_user unless @logged_in_user&.admin
-    respond_to do |format|
+    check_user {
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        redirect_to @user, notice: 'User was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
+    }
   end
 
   # DELETE /users/1
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to users_url, notice: 'User was successfully destroyed.' 
   end
 
   private
   def check_user
     if !@logged_in_user
-      redirect_to login_path
-      return
-    end
-
-    if !@user
       redirect_to login_path
       return
     end
