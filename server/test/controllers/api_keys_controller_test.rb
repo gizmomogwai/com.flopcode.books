a1ba6e94ef32fbe74915d95cc81e5f0fdea02f25
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-23 19:12:17 +0100

    added tests for show api_keys
    work on #8

--- server/test/controllers/api_keys_controller_test.rb
+++ server/test/controllers/api_keys_controller_test.rb
@@ -25,4 +25,27 @@ class ApiKeysControllerTest < ActionController::TestCase
       block.call(self, users(:normal2))
     end
   end
+
+  {
+    normal: -> (x) {
+      x.assert_response :success
+    },
+    normal2: -> (x) {
+      x.assert_redirected_to x.login_path
+    },
+    admin: -> (x) {
+      x.assert_response :success
+    }
+  }.each do |user, block|
+    test "show api key for other user as '#{user}' users" do
+      normal_user = users(:normal)
+      normal_user.create_api_key("test")
+      key = normal_user.api_keys.first
+
+      login_as(user)
+
+      get :show, id: key.id, user_id: users(:normal).id
+      block.call(self)
+    end
+  end
 end
