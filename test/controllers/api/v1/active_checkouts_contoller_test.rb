090c50f08ad564eb3582d7b2f091a115fd99ba25
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-23 13:20:26 +0100

    added more tests for api_keys_controller
    work on #8

--- test/controllers/api/v1/active_checkouts_contoller_test.rb
+++ test/controllers/api/v1/active_checkouts_contoller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Api::V1::ActiveCheckoutsControllerTest < ActionController::TestCase
-#  test "active checkout can be created with a user" do
-#    post :create, active_checkout: {test: test}
-#  end
+  #  test "active checkout can be created with a user" do
+  #    post :create, active_checkout: {test: test}
+  #  end
 end
