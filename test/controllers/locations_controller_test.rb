cf95db4ff7d9055587b718681a7fe80cbbad4999
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-26 11:22:08 +0100

    added tests for show locations
    for #9

--- test/controllers/locations_controller_test.rb
+++ test/controllers/locations_controller_test.rb
@@ -19,6 +19,19 @@ class LocationsControllerTest < ActionController::TestCase
     end
   end
 
+  {
+    nil => -> (x) {
+      x.assert_redirected_to x.login_path
+    },
+    normal: -> (x) {
+      x.assert_response :success
+    }
+  }.each do |user, block|
+    test "show location for '#{user}' users" do
+      login_as(user)
+      get :show, id: locations(:one).id
+    end
+  end
 
   test "should only answer html" do
     assert_raises_with_message(ActionController::UnknownFormat, 'ActionController::UnknownFormat') do
