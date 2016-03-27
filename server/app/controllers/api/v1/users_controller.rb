d8e23f38ac2787523cc40a3e59a3af711cb593ad
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-27 14:17:06 +0200

    added users api controller test
    for #1

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,11 @@
 class Api::V1::UsersController < Api::ApiController
   respond_to :json
-  #before_filter :authenticate
-  before_filter :set_user, only: [:show]
 
-  def show
-    respond_with @user
+  before_filter :authenticate
+
+  def index
+    @users = User.all
+    respond_with @users
   end
 
-  private
-  def set_user
-    @user = User.find(params[:id])
-  end
 end
