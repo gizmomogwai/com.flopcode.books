9254d0c85a7f3801ce107d636eb10b5c07f79a03
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-03-28 14:22:41 +0200

    added setup for frontend tests
    for frontend tests #8

--- server/Rakefile
+++ server/Rakefile
@@ -4,3 +4,9 @@
 require File.expand_path('../config/application', __FILE__)
 
 Rails.application.load_tasks
+
+desc 'run test server with fresh db'
+task :run_testserver do
+  sh "RAILS_ENV=test rake db:drop db:schema:load db:seed"
+  sh "RAILS_ENV=test rails s"
+end
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def seed_test_db
+  locations = Location.create([
+                                {name: 'l-one', description: 'desc 1'},
+                                {name: 'l-two', description: 'desc 2'}
+                              ])
+  users = User.create([
+                        {name: 'name1', account: 'account1', admin: true},
+                        {name: 'name2', account: 'account2', admin: false}
+                      ])
+  books = Book.create([
+                        {isbn: 'isbn1', title: 'title1', authors: 'authors1', user: users[0], location: locations[0]},
+                        {isbn: 'isbn2', title: 'title2', authors: 'authors2', user: users[1], location: locations[1]}
+                      ])
+  api_keys = ApiKey.create([
+                             {key: 'key1', for_app: 'app1', user: users[0]},
+                             {key: 'key2', for_app: 'app2', user: users[1]}
+                           ])
+end
+
+case Rails.env
+when 'test'
+  seed_test_db
+else
+end
