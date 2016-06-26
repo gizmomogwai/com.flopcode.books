b46fa506fd91a333ff5f8fc1ad416995d0b3fdd0
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-06-26 18:34:32 +0200

    removed dependency on git

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -2,7 +2,7 @@
 
 require File.expand_path('../application', __FILE__)
 
-APP_VERSION = `git describe --always`.strip unless defined? APP_VERSION
+APP_VERSION = '0.0.1'#`git describe --always`.strip unless defined? APP_VERSION
 
 # Initialize the Rails application.
 Rails.application.initialize!
