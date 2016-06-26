71002bfb0fb205429c501af7bc4fe3160c25d192
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-06-26 23:55:40 +0200

    shorter revision for production server

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 
 REVISION_FILE = File.join(Rails.root, 'REVISION')
 if File.exist?(REVISION_FILE)
-  APP_VERSION = File.read(REVISION_FILE)
+  APP_VERSION = File.read(REVISION_FILE)[0...7]
 else
   APP_VERSION = `git describe --always`.strip unless defined? APP_VERSION
 end
